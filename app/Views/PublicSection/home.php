839cece7f7fb62f0761460149d34430fdc4e5ee6
Hector Plinio <hectorplinio@MacBook-Air-de-Hector.local>
2021-11-26 20:52:27 +0100

    Haciendo css parte home

--- app/Views/PublicSection/home.php
+++ app/Views/PublicSection/home.php
@@ -1,27 +1,51 @@
 <?= $this->extend('PublicSection/base_layout') ?>
     <?= $this->section('css') ?>
-        <link href="<?= base_url('assets/PublicSection/css/login.css')?>"rel="stylesheet" type="text/css">
+        <link href="<?= base_url('assets/PublicSection/css/home.css')?>"rel="stylesheet" type="text/css">
     <?= $this->endSection() ?>
     <?= $this->section('js') ?>
         <script type="text/javascript">
             $(document).ready(function(){  
-                console.log("Pantalla de Login")
+                console.log("Pantalla de Home")
             });
         </script>
     <?= $this->endSection() ?>
     <?= $this->section('section') ?>
         <div class="contenedor">
-            <div class="logo">
-                <img src="<?= base_url('assets/PublicSection/img/logo.png')?>">
+            <div class="titulos">
+                <h1 class="titulo">Festivales</h1>
             </div>
-                <h1 class="titulo">Please sign in</h1>
-                <form class="formulario">
-                    <input type="text" class="form-control" placeholder="Email address">
-                    <input type="pass" class="form-control" placeholder="Password">
-                    <button class="btn btn-primary" id="boton">Sign in</button>
-                </form>
-                <p id="fecha">©️&nbsp2017-2021</p>
-                <div class="enlaces">
+            <h1 class="titulo">Indie</h1>
+            <div class="festival">
+                <div class="tarjeta">
+                        <img src="<?= base_url('assets/PublicSection/img/arts.jpeg')?>">
+                    <div class="info">
+                        <h3>Card title</h3>
+                        <p>Some quick example text to build on the card title and make up the builk of the card´s content.</p>
+                        <button class="btn btn-primary" id="boton">Go somewhere</button>
+                    </div>
+                </div>
+                <div class="tarjeta">
+                        <img src="<?= base_url('assets/PublicSection/img/arts.jpeg')?>">
+                    <div class="info">
+                        <h3>Card title</h3>
+                        <p>Some quick example text to build on the card title and make up the builk of the card´s content.</p>
+                        <button class="btn btn-primary" id="boton">Go somewhere</button>
+                    </div>
+                </div>
+            </div>
+            <h1 class="titulo">Rock</h1>
+
+            <div class="festival">
+                <div class="tarjeta">
+                    <img src="<?= base_url('assets/PublicSection/img/rock.jpeg')?>">
+                    <div class="info">
+                        <h3>Card title</h3>
+                        <p>Some quick example text to build on the card title and make up the builk of the card´s content.</p>
+                        <button class="btn btn-primary" id="boton">Go somewhere</button>
+                    </div>
+                </div>
+            </div>
+                    <div class="enlaces">
                 <a href="<?= route_to('login_page') ?>">Ir a login</a>
                 </div>
             
